f140be30995686f1ec65e46416f8847b89a8d91d
britikov <britikov@N-W11-015>
2024-01-24 11:31:00 +0100

    init

---
+++ RadTreeViewBehavior/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RadTreeViewBehavior
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            Projects = new ObservableCollection<ProjectItem>()
+            {
+                new ProjectItem()
+                {
+                    Name = $"Project-{Guid.NewGuid().ToString()}",
+                    Files = new ObservableCollection<FileItem>()
+                    {
+                        new FileItem() { Name = $"File-{Guid.NewGuid().ToString()}", },
+                        new FileItem() { Name = $"File-{Guid.NewGuid().ToString()}", },
+                        new FileItem() { Name = $"File-{Guid.NewGuid().ToString()}", },
+                    }
+                }
+            };
+            this.DataContext = this;
+            InitializeComponent();
+        }
+
+        public ObservableCollection<ProjectItem> Projects { get; }
+
+        private void RadTreeView_Edited(object sender, Telerik.Windows.Controls.RadTreeViewItemEditedEventArgs e)
+        {
+            if (object.Equals(e.OldValue, e.NewValue))
+                MessageBox.Show("OldValue same as NewValue");
+
+        }
+    }
+}
---
+++ RadTreeViewBehavior/ProjectItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadTreeViewBehavior
+{
+    public class ProjectItem
+    {
+        public string Name { get; set; } = string.Empty;
+        public ObservableCollection<FileItem> Files { get; set; } = new ObservableCollection<FileItem>();
+    }
+}
